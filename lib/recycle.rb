e3f1f9cc8ffba48dead2038ee9d90dc3c427fa4b
margaretmeyers <marg.m.meyers@gmail.com>
2018-08-09 16:43:42 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -27,14 +27,11 @@ puts address(first_recycle_bin)
 # This method should return the value of the borough key in the hash.
 
 def borough(first_recycle_bin) 
-  first_recycle_bin =  {
-  :address => "227 St/Bronx River Pkway",
-  :site_type =>"Subproperty",
-  :longitude =>"-73.864223918",
-  :borough =>"Bronx",
-  :latitude =>"40.890848989",
-  :park_site_name =>"227th St. Plgd
-}
+ return first_recycle_bin["borough"]
+end
+puts address(first_recycle_bin)
+  
+
 
 #3. Define a method called `all_keys` that takes a hash as an argument.
 #This method should print out each key in the hash. 
